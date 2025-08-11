88150317d19cd33871bc2962b1bd1849861a8594
Brayan Ruiz <55265523+BrayanRuiz@users.noreply.github.com>
2025-08-11 14:09:57 -0600

    🎉tada(Reforzamiento) : Sección terminada

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,9 @@ import './style.css'
 // import './bases/06-obj-destructuring';
 // import './bases/07-array-destructuring';
 // import './bases/08-imp-exp';
-import './bases/09-promises';
+// import './bases/09-promises';
+// import './bases/10-fetch-api';
+import './bases/11-async-await';
 
 document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <div>
